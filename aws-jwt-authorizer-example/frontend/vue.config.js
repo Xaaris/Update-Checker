3e2d4964095ce53f51bd602b74c421ae29106a80
johannes.berger <johannes.berger@xing.com>
2021-11-24 09:25:05 +0100

    Renamed service

--- aws-jwt-authorizer-example/frontend/vue.config.js
+++ aws-jwt-authorizer-example/frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'https://j7m8bn25ud.execute-api.eu-central-1.amazonaws.com',
+                target: 'https://42p84bzsld.execute-api.eu-central-1.amazonaws.com/',
                 changeOrigin: true
             },
         }
